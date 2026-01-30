dd7b2ddab6f706d054891a4107a8b5b41f60808b
mohammedelsiredu-ops <mohammedelsir.edu@gmail.com>
2026-01-30 13:07:45 +0100

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: './public/index.html'
    //   favicon: './public/images/favicon.ico'
     })
   ],
